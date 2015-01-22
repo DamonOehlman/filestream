6a4bc73c067f2cff927dd853f12472e865d3026a
Damon Oehlman <damon.oehlman@gmail.com>
2015-01-22 11:48:45 +1100

    Refactor filewritestream to allow headers to be processed down stream

--- write.js
+++ write.js
@@ -18,7 +18,7 @@ function FileWriteStream(callback, opts) {
   }, opts));
 
   // when the stream finishes create a file
-  this.on('finish', this._createFile.bind(this));
+  this.on('finish', this._generateFile.bind(this));
 
   // create the internal buffers storage
   this._buffers = [];
@@ -30,57 +30,50 @@ util.inherits(FileWriteStream, Writable);
 module.exports = FileWriteStream;
 
 FileWriteStream.prototype._createFile = function() {
-  var file;
-
   // if we have no buffers, then abort any processing
   if (this._buffers.length === 0) {
     return;
   }
 
-  // create the new file
-  file = this.metadata ?
-    // if we have metadata, then create a file with name and metadata
-    new File([new Blob(this._buffers)], this.metadata.name, this.metadata) :
-    // otherwise, just create the file
-    new File([new Blob(this._buffers)], '');
+  return new File([new Blob(this._buffers)], '');
+};
 
-  if (typeof this.callback == 'function') {
-    this.callback(file, this.metadata);
+FileWriteStream.prototype._generateFile = function() {
+  var file = this._createFile();
+
+  if (file) {
+    if (typeof this.callback == 'function') {
+      this.callback(file);
+    }
+
+    this.emit('file', file);
   }
 
-  this.emit('file', file, this.metadata);
-
-  // reset the buffers and metadata
+  // reset the buffers and counters
   this._buffers = [];
-  this.metadata = null;
   this._bytesreceived = 0;
 };
 
+FileWriteStream.prototype._preprocess = function(data, callback) {
+  // pass through the data
+  callback(null, data);
+};
+
 FileWriteStream.prototype._write = function(chunk, encoding, callback) {
-  var parts = typeof chunk == 'string' && chunk.split('|');
   var data = Buffer.isBuffer(chunk) ? chunk : toBuffer(chunk);
+  var writeStream = this;
 
-  // if this is the metadata line, then update our metadata
-  if (parts && parts[0] === 'meta') {
-    try {
-      this.metadata = JSON.parse(parts[1]);
-    }
-    catch (e) {
-      this.emit('error', 'Could not deserialize metadata');
+  this._preprocess(data, function(err, processed) {
+    if (err) {
+      return callback(err);
     }
 
-    return callback();
-  }
+    // if the incoming data has been passed through, then add to the bytes received buffer
+    if (processed) {
+      writeStream._bytesreceived += processed.length;
+      writeStream._buffers.push(processed);
+    }
 
-  // if we have valid data, then process
-  if (Buffer.isBuffer(data)) {
-    this._bytesreceived += chunk.length;
-    this._buffers.push(chunk);
-  }
-
-  if (this.metadata && this._bytesreceived >= this.metadata.size) {
-    this._createFile();
-  }
-
-  callback();
+    callback();
+  });
 };
