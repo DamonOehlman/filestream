eacc090039986c92fc3a9182f75fc1399d08bc40
Jimmy Wärting <jimmy@warting.se>
2021-07-03 02:24:31 +0200

    always depend on newest buffer + stream version

--- write.js
+++ write.js
@@ -1,74 +1,50 @@
 /* global File */
 
-const { Writable } = require('readable-stream')
-const toBuffer = require('typedarray-to-buffer')
+const { Writable } = require('stream')
+const { Buffer } = require('buffer')
 
 class FileWriteStream extends Writable {
-  constructor (callback, opts) {
+  constructor (callback, opts = {}) {
     // inherit writable
     super(Object.assign({ decodeStrings: false }, opts))
 
-    // when the stream finishes create a file
-    this.on('finish', this._generateFile.bind(this))
-
+    this._bytesreceived = 0
     // create the internal buffers storage
     this._buffers = []
-    this._bytesreceived = 0
+    // when the stream finishes create a file
+    this.once('finish', () => this._generateFile())
+
     this.callback = callback
-    this.type = (opts || {}).type
-  }
-
-  _createFile () {
-    // if we have no buffers, then abort any processing
-    if (this._buffers.length === 0) {
-      return
-    }
-
-    return new File(this._buffers, '', {
-      type: this.type || ''
-    })
+    this.type = opts.type || ''
   }
 
   _generateFile () {
-    const file = this._createFile()
+    const file = this._buffers.length && new File(this._buffers, '', { type: this.type })
 
     if (file) {
-      if (typeof this.callback === 'function') {
-        this.callback(file)
-      }
-
+      this.callback?.(file)
       this.emit('file', file)
     }
 
     // reset the buffers and counters
-    this._buffers = []
-    this._bytesreceived = 0
-  }
-
-  _preprocess (data, callback) {
-    // pass through the data
-    callback(null, data)
+    this._buffers = null
+    this._bytesreceived = null
   }
 
   _write (chunk, encoding, callback) {
-    const data = Buffer.isBuffer(chunk) ? chunk : toBuffer(chunk)
-    const writeStream = this
+    const data = Buffer.isBuffer(chunk)
+      ? chunk
+      : Buffer.from(chunk.buffer, chunk.byteOffset, chunk.byteLength)
 
-    this._preprocess(data, (err, processed) => {
-      if (err) {
-        return callback(err)
-      }
+    // if the incoming data has been passed through,
+    // then add to the bytes received buffer
+    if (data) {
+      this._bytesreceived += data.length
+      this._buffers.push(data)
+      this.emit('progress', this._bytesreceived)
+    }
 
-      // if the incoming data has been passed through,
-      // then add to the bytes received buffer
-      if (processed) {
-        writeStream._bytesreceived += processed.length
-        writeStream._buffers.push(processed)
-        writeStream.emit('progress', writeStream._bytesreceived)
-      }
-
-      callback()
-    })
+    callback()
   }
 }
 
